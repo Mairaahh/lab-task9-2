65e33d0693a75ef8b0f609812e4dbd1c3315f9b8
Mairaahh <101237565+Mairaahh@users.noreply.github.com>
2023-02-22 17:38:57 +0800

    Lab-task9

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyDvoivBgGBFdDfm1Md5oupMnI4GasXLSAQ",
+  authDomain: "lab-task9-7216b.firebaseapp.com",
+  projectId: "lab-task9-7216b",
+  storageBucket: "lab-task9-7216b.appspot.com",
+  messagingSenderId: "98260267799",
+  appId: "1:98260267799:web:14372280bf15dce1fa3d52",
+  measurementId: "G-C0VJC51CM9" 
+  }
+}
